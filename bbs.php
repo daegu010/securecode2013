ce818cf06d9e4001ec2c78c01b2e8b7fc021b27e
eurekasolution <kjmin@daum.net>
2022-12-13 14:03:23 +0900

    B1403

--- bbs.php
+++ bbs.php
@@ -6,7 +6,21 @@
 
     if($mode == "list")
     {
-        echo "목록보기";
+        ?>
+        <div class="row">
+            <div class="col-1">순서</div>
+            <div class="col-9">제목</div>
+            <div class="col-2">작성자</div>
+        </div>
+
+
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-primary btn-sm" onClick="location.href='main.php?cmd=bbs&mode=write' ">글쓰기</button>
+            </div>
+        </div>
+
+        <?php
     }
 
     if($mode == "write")
