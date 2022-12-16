bc4307f19e8290c9f35aab983dcf513eeca11ccf
eurekasolution <kjmin@daum.net>
2022-12-16 14:59:05 +0900

    E1500

--- log.php
+++ log.php
@@ -1,4 +1,34 @@
 <?php
+    function ip2country($ip)
+    {
+        // ip = 1.2.3.4
+        $splitIP = explode(".", $ip);
+        //echo "ip_files/" .$splitIP[0] . ".php";
+        include "ip_files/" .$splitIP[0] . ".php";
+        
+        $code =($splitIP[0] * 16777216) + ($splitIP[1] *65536) + ($splitIP[2] * 256) + ($splitIP[3]);
+        foreach($ranges as $key => $value)
+        {
+            if($key <= $code)
+            {
+                if($ranges[$key][0] >= $code)
+                    $country = $ranges[$key][1]; break;
+            }
+        }
+
+        if(!isset($country))
+            $country = "";
+
+        if(isset($country) and $country == "")
+        {
+            $country = "noflag";
+        }
+
+
+        return $country;
+    }
+
+
     $sql = "select * from logs order by idx desc limit 50";
     $result = mysqli_query($conn, $sql);
     $data = mysqli_fetch_array($result);
@@ -10,6 +40,7 @@
     <div class="col-1">순서</div>
     <div class="col-2">이름</div>
     <div class="col-2">IP</div>
+    <div class="col-1">국가</div>
     <div class="col">작업</div>
     <div class="col">날짜</div>
 </div>
@@ -17,11 +48,14 @@
 <?php
     while($data)
     {
+        $nation = ip2country($data["ip"]);
+        $nationFlag = "<img src='flags/$nation.gif'>";
         ?>
         <div class="row">
             <div class="col-1"><?php echo $data["idx"] ?></div>
             <div class="col-2"><?php echo $data["name"] ?></div>
             <div class="col-2"><?php echo $data["ip"] ?></div>
+            <div class="col-1"><?php echo $nationFlag ?></div>
             <div class="col"><?php echo $data["work"] ?></div>
             <div class="col"><?php echo $data["time"] ?></div>
         </div>
