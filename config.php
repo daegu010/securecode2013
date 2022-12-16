a81ff616074bd0bee0ea3f36a9fe23d15c11e34c
eurekasolution <kjmin@daum.net>
2022-12-16 10:28:05 +0900

    E1028

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
     $site_title = "사이트 타이틀 테스트";
     $pw_prefix = "as&dkd@3";
+    $phone1 = "010";
+    $phone2 = "3540";
+    $phone3 = "8848";
+    $smsAuthKey = "35954e0b31ce614f01593a5046a79fd5";
 ?>
